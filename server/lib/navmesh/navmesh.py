58142339de845706b02b4e161ea77536350d08dc
Kevin Chou <kchou@tinyco.com>
2013-11-11 19:52:20 -0800

    pathfinding work

--- server/lib/navmesh/navmesh.py
+++ server/lib/navmesh/navmesh.py
@@ -27,14 +27,7 @@ class Navmesh(object):
 				if True or not intersects(node, polygons):
 					node_a.neighbors.append(node_b)
 
-	def find_path(self, x1, y1, x2, y2):
-		self.compute_neighbours()
-
-		start_node = Node(x1, y1)
-		goal_node = Node(x2, y2)
-
-		# check for direct los from start_node to goal_node
-
+	def get_closest_node(self, node):
 		min_dist = distance_between(start_node, self.nodes[0])
 		min_node = self.nodes[0]
 
@@ -46,14 +39,26 @@ class Navmesh(object):
 
 		closest_node = min_node
 
+	def find_path(self, x1, y1, x2, y2):
+		self.compute_neighbours()
+
+		start_node = Node(x1, y1)
+		goal_node = Node(x2, y2)
+
+		# check for direct los from start_node to goal_node
+
+		closest_node = get_closest_node(start_node)
+
 		closed_set = set()
 		open_queue = []
+
+		# key: node, value: previous_node_in_path
 		path_map = {}
 		heappush(open_queue, (0, closest_node))
 
 		while len(open_queue) > 0:
 			current_node_cost, current_node = heappop(open_queue)
-			if current_node.x == goal_node.x and current_node.y == goal_node.y:
+			if current_node == goal_node:
 				break
 
 			neighbors = current_node.neighbors
@@ -75,5 +80,13 @@ class Navmesh(object):
 
 			closed_set.add(current_node)
 
+		path_node = goal_node
+		path = []
+
+		while path_node in path_map:
+			path.append(path_node)
+			path_node = path_map[path_node]
+
+		return path
 		
 
--- server/lib/navmesh/node.py
+++ server/lib/navmesh/node.py
@@ -3,3 +3,9 @@ class Node(object):
 		self.x = x
 		self.y = y
 		self.neighbors = []
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.x == other.x and self.y == other.y
+
+        return False

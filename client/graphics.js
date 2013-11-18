cf081acb0453b7138125f0711eb59d2b281f3664
Yufei Guo <this.is.yufei@gmail.com>
2013-11-17 18:57:04 -0800

    test model

--- client/graphics.js
+++ client/graphics.js
@@ -1,5 +1,8 @@
 var camera, scene, renderer, controls;
 var geometry, material, mesh, land;
+
+var objects = {};
+
 var time = Date.now();
 
 $(document).ready(function(){
@@ -22,11 +25,12 @@ function init() {
 
     mesh = new THREE.Mesh( geometry, material );
     mesh.position.set(0,200,-700);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
     scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer({canvas: canvas, antialias:true});
-
-
+    renderer.shadowMapEnabled = true;
     // set up the sphere vars
     var radius = 50,
         segments = 30,
@@ -47,44 +51,72 @@ function init() {
         segments,
         rings),
         sphereMaterial);
+    sphere.castShadow = true;
 
     // add the sphere to the scene
     scene.add(sphere);
 
     sphere.position.x = 0;
-    sphere.position.y = 50;
+    sphere.position.y = 500;
     sphere.position.z = -500;
 
     // create a point light
-    var pointLight =
-      new THREE.PointLight(0xFFFFFF);
+   // var pointLight =
+   //   new THREE.PointLight(0xFFFFFF);
 
     // set its position
-    pointLight.position.x = 10;
-    pointLight.position.y = 40;
-    pointLight.position.z = 130;
+   // pointLight.position.x = 10;
+   // pointLight.position.y = 40;
+    //pointLight.position.z = 130;
 
     // add to the scene
-    scene.add(pointLight);
+   // scene.add(pointLight);
 
     var landgeometry = new THREE.CubeGeometry( 5000, 100, 5000 );
     var landmaterial = new THREE.MeshLambertMaterial( { color: 0xCCCCCC} );
     land = new THREE.Mesh(landgeometry, landmaterial);
     land.position.set(0,-50,-2500);
+    land.receiveShadow = true;
     scene.add(land);
 
     //var hemLight = new THREE.HemisphereLight(0x0000FF, 0x00FF00, 0.5);
    //scene.add(hemLight);
 
-    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-    directionalLight.position.set( 0, 100, -100 );
-    scene.add( directionalLight );
+   // var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    //directionalLight.position.set( 0, 100, -100 );
+    //scene.add( directionalLight );
 
    // var pointer = new THREE.PointerLockControls(camera);
     //pointer.enabled = true;
  //   scene.add(pointer);
     controls = new THREE.OrbitControls( camera );
    // controls.addEventListener( 'change', render );
+
+   var light = new THREE.SpotLight( 0xffffff, 1, 0, Math.PI, 1 );
+    light.position.set( 0, 1500, 1000 );
+    light.target.position.set( 0, 0, -500 );
+
+    light.castShadow = true;
+
+    light.shadowCameraNear = 700;
+    light.shadowCameraFar = camera.far;
+    light.shadowCameraFov = 50;
+
+    //light.shadowCameraVisible = true;
+
+    light.shadowBias = 0.0001;
+    light.shadowDarkness = 0.5;
+
+    light.shadowMapWidth = 2048;
+    light.shadowMapHeight = 1024;
+    scene.add( light );
+
+
+    var ambient = new THREE.AmbientLight( 0x444444 );
+    scene.add( ambient );
+
+    createMeshFromFile("testObject.js", "textObject");
+
 }
 
 function animate() {
@@ -94,6 +126,12 @@ function animate() {
 
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
+
+    var testObject = objects["textObject"];
+    if (testObject !== undefined) {
+        testObject.rotation.x += 0.01;
+         testObject.rotation.y += 0.02;
+    }
 
     var now = Date.now();
    // console.log (now + " " + time);
@@ -112,5 +150,23 @@ function animate() {
 
 function render() {
     renderer.render( scene, camera );
+
+}
+
+
+function createMeshFromFile(filename, objectID) {
+    var loader = new THREE.JSONLoader();
+    loader.load( "models/"+filename, function(geometry, material){
+      console.log("Loaded "+filename + " with geometry " + geometry + " and material "+ material);
+     // var material = new THREE.MeshLambertMaterial({color: 0x666666});
+      var filemesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(
+        material ) );
+      filemesh.position.set(0, 400, -300);
+        filemesh.scale.set( 50, 50, 50);
+        filemesh.castShadow = true;
+
+      scene.add(filemesh);
+      objects[objectID] = filemesh;
+    });
 
 }

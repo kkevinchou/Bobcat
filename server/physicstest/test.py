97fa1a6935b1fefc78103f5c2cc9a097c43e0eed
Kevin C <kkevinchou@gmail.com>
2013-11-23 15:03:06 -0800

    physics stuff

--- server/physicstest/test.py
+++ server/physicstest/test.py
@@ -1,111 +1,78 @@
-from direct.showbase.ShowBase import ShowBase
+from direct.directtools.DirectGeometry import LineNodePath
 from panda3d.core import *
-from panda3d.physics import *
+from panda3d.ode import *
+from direct.showbase.ShowBase import ShowBase
 import sys
-from direct.showbase.DirectObject import DirectObject
-
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.cam.setPos(0, -50, 10)
-        self.setupCD()
-        self.setupPhysics()
-        self.addSmiley()
-        self.addFloor()
+        self.lines = LineNodePath(parent = render, thickness = 3.0, colorVec = Vec4(1, 0, 0, 1))
+
+        # Load the self.smiley and self.frowney models
+        self.smiley = loader.loadModel("smiley.egg")
+        self.smiley.reparentTo(render)
+        self.smiley.setPos(-5, 0, -5)
+        self.frowney = loader.loadModel("frowney.egg")
+        self.frowney.reparentTo(render)
+        self.frowney.setPos(-12.5, 0, -7.5)
+         
+        # Setup our physics self.world
+        self.world = OdeWorld()
+        self.world.setGravity(0, 0, -9.81)
+         
+        # Setup the body for the self.smiley
+        self.smileyBody = OdeBody(self.world)
+        M = OdeMass()
+        M.setSphere(5000, 1.0)
+        self.smileyBody.setMass(M)
+        self.smileyBody.setPosition(self.smiley.getPos(render))
+        self.smileyBody.setQuaternion(self.smiley.getQuat(render))
+         
+        # Now, the body for the self.frowney
+        self.frowneyBody = OdeBody(self.world)
+        M = OdeMass()
+        M.setSphere(5000, 1.0)
+        self.frowneyBody.setMass(M)
+        self.frowneyBody.setPosition(self.frowney.getPos(render))
+        self.frowneyBody.setQuaternion(self.frowney.getQuat(render))
+         
+        # Create the joints
+        smileyJoint = OdeBallJoint(self.world)
+        smileyJoint.attach(self.smileyBody, None) # Attach it to the environment
+        smileyJoint.setAnchor(0, 0, 0)
+        frowneyJoint = OdeBallJoint(self.world)
+        frowneyJoint.attach(self.smileyBody, self.frowneyBody)
+        frowneyJoint.setAnchor(-5, 0, -5)
+         
+        # Set the camera position
+        base.disableMouse()
+        base.camera.setPos(0, 50, -7.5)
+        base.camera.lookAt(0, 0, -7.5)
 
         self.accept("escape", sys.exit)
-        self.accept("arrow_left", self.setKey, ["left", 1])
-        self.accept("arrow_right", self.setKey, ["right", 1])
-        self.accept("arrow_up", self.setKey, ["forward", 1])
-        self.accept("arrow_left-up", self.setKey, ["left", 0])
-        self.accept("arrow_right-up", self.setKey, ["right", 0])
-        self.accept("arrow_up-up", self.setKey, ["forward", 0])
 
-        self.keyMap = {}
-        self.keyMap['left'] = 0
-        self.keyMap['right'] = 0
-        self.keyMap['forward'] = 0
+        taskMgr.doMethodLater(0.5, self.simulationTask, "Physics Simulation")
 
-        taskMgr.add(self.move,"moveTask")
+    def drawLines(self):
+        # Draws lines between the self.smiley and self.frowney.
+        self.lines.reset()
+        self.lines.drawLines([((self.frowney.getX(), self.frowney.getY(), self.frowney.getZ()),
+                        (self.smiley.getX(), self.smiley.getY(), self.smiley.getZ())),
+                       ((self.smiley.getX(), self.smiley.getY(), self.smiley.getZ()),
+                        (0, 0, 0))])
+        self.lines.create()
 
-    def setKey(self, key, value):
-        self.keyMap[key] = value
-
-    def move(self, task):
-        if (self.keyMap["left"]):
-            pass
-            # self.ralph.setH(self.ralph.getH() + 300 * globalClock.getDt())
-        if (self.keyMap["right"]!=0):
-            pass
-            # self.ralph.setH(self.ralph.getH() - 300 * globalClock.getDt())
-        if (self.keyMap["forward"]!=0):
-            self.phys_object.set_velocity(Vec3(0, 10, 0))
-            # self.ralph.setY(self.ralph, -25 * globalClock.getDt())
-
+    # The task for our simulation
+    def simulationTask(self, task):
+        # Step the simulation and set the new positions
+        # self.drawLines()
+        self.world.quickStep(globalClock.getDt())
+        self.frowney.setPosQuat(render, self.frowneyBody.getPosition(), Quat(self.frowneyBody.getQuaternion()))
+        self.smiley.setPosQuat(render, self.smileyBody.getPosition(), Quat(self.smileyBody.getQuaternion()))
+        self.drawLines()
         return task.cont
 
-
-    def thrust(self):
-        thrustNode = ForceNode("thrust")
-        self.phys.attachNewNode(thrustNode)
-        self.thrustForce = LinearVectorForce(-100, 0, 0)
-        self.thrustForce.setMassDependent(1)
-        thrustNode.addForce(self.thrustForce)
-        base.physicsMgr.addLinearForce(self.thrustForce)
-
-    def setupCD(self):
-        base.cTrav = CollisionTraverser()
-        base.cTrav.showCollisions(render)
-        self.notifier = CollisionHandlerEvent()
-        self.notifier.addInPattern("%fn-in-%in")
-        self.notifier.addOutPattern("%fn-out-%in")
-        self.accept("smiley-in-floor", self.onCollisionStart)
-        self.accept("smiley-out-floor", self.onCollisionEnd)
-
-        self.pusher = PhysicsCollisionHandler()
-
-
-    def setupPhysics(self):
-        base.enableParticles()
-        gravNode = ForceNode("gravity")
-        render.attachNewNode(gravNode)
-        gravityForce = LinearVectorForce(0, 0, -9.81)
-        gravNode.addForce(gravityForce)
-        base.physicsMgr.addLinearForce(gravityForce)
-
-    def addSmiley(self):
-        self.actor = ActorNode("physics")
-        self.phys_object = self.actor.getPhysicsObject()
-        self.phys_object.setMass(10)
-
-        self.phys = render.attachNewNode(self.actor)
-        base.physicsMgr.attachPhysicalNode(self.actor)
-
-        self.smiley = loader.loadModel("smiley")
-        self.smiley.reparentTo(self.phys)
-        self.phys.setPos(0, 0, 10)
-
-        col = self.smiley.attachNewNode(CollisionNode("smiley"))
-        col.node().addSolid(CollisionSphere(0, 0, 0, 1.1))
-        col.show()
-
-        self.pusher.addCollider(col, self.phys)
-        base.cTrav.addCollider(col, self.pusher)
-
-    def addFloor(self):
-        floor = render.attachNewNode(CollisionNode("floor"))
-        floor.node().addSolid(CollisionPlane(Plane(Vec3(0, 0, 1),
-        Point3(0, 0, 0))))
-        floor.show()
-
-    def onCollisionStart(self, entry):
-        print 'Collision Start'
-        # base.physicsMgr.addLinearForce(self.thrustForce)
-
-    def onCollisionEnd(self, entry):
-        print 'Collision End'
-        # base.physicsMgr.removeLinearForce(self.thrustForce)
-
 app = Application()
+app.drawLines()
 app.run()
